6f2b127de3f776f5c59538117e474de365d6929d
Oliver <olsfinest@gmail.com>
2020-01-02 18:24:16 +0800

    second commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+
 
 import Header from './Header';
 import OverlayMenu from './OverlayMenu';
@@ -8,7 +8,7 @@ import Hamburger from './Hamburger';
 import Footer from './Footer';
 
 import './layout.css';
-import 'bootstrap/dist/css/bootstrap-grid.css';
+
 
 const Primary = styled.main`
   padding: 110px 0 0 0;
