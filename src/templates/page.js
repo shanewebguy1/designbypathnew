86c54df50995e04de1679da3c12b4dec6f525aa8
Oliver <olsfinest@gmail.com>
2020-01-02 17:59:04 +0800

    first commit

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {   graphql } from 'gatsby';
-import Layout from '../components/Layout';
+
 
 
 const PageTemplate = ({ data }) => (
 
-<Layout>
+
 <div className="eqweq">
 eqwewq
 
 </div>
-</Layout>
+
 
 )
 
